dcdb294982a25235081f1a682969b46544160b00
Alex Luschan <alexander.luschan@avl.com>
2018-05-17 08:27:01 +0200

    housekeeping in fmi.Lab environment

--- updateSrc_CombEng.m
+++ updateSrc_CombEng.m
@@ -1,4 +1,5 @@
-!xcopy E:\AVL\buildEnv\workspace\CombEng\*.cpp /Y /F
-!xcopy E:\AVL\buildEnv\workspace\CombEng\*.h /Y /F
+%git repo!!!
+%!xcopy E:\AVL\buildEnv\workspace\CombEng\*.cpp /Y /F
+%!xcopy E:\AVL\buildEnv\workspace\CombEng\*.h /Y /F
 
 !del CombEng_avl_artmi_intime6_rtw\*.obj
